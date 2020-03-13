89a80a163923d7e0a98d92886f9b00699c7e43f2
Remy van Berkum <remy.van.berkum@enterprisemodules.com>
2020-03-13 14:43:43 +0100

    [vagrant] Disable dhcp_fix and sync files from other demo's
    Change-Id: I5d39da91544b72767c3cac44f529cc2adca72817

--- vm-scripts/install_puppet.sh
+++ vm-scripts/install_puppet.sh
@@ -1,12 +1,20 @@
-#
-# Install correct puppet version
-#
-if [ -f "/vagrant/puppet_version" ]; then
-  PACKAGE="puppet-agent-$(cat /vagrant/puppet_version)"
+if [ -f /var/log/install_puppet.done ]
+then
+  echo "Puppet already installed"
 else
-  PACKAGE="puppet-agent"
+  #
+  # Install correct puppet version
+  #
+  if [ -f "/vagrant/puppet_version" ]; then
+    PACKAGE="puppet-agent-$(cat /vagrant/puppet_version)"
+  else
+    PACKAGE="puppet-agent"
+  fi
+  echo "Installing $PACKAGE"
+  rhel=$(awk -F'[ .]' 'NF==8{print $4} NF==9{print $7}' /etc/redhat-release)
+  yum install -y --nogpgcheck https://yum.puppetlabs.com/puppet6/puppet6-release-el-${rhel}.noarch.rpm > /dev/null
+  yum install -y --nogpgcheck $PACKAGE
+  rpm -q git || yum install -y --nogpgcheck git
+
+  touch /var/log/install_puppet.done
 fi
-echo "Installing $PACKAGE"
-yum install -y --nogpgcheck https://yum.puppetlabs.com/puppet6/puppet6-release-el-7.noarch.rpm > /dev/null
-yum install -y --nogpgcheck $PACKAGE
-rpm -q git || yum install -y --nogpgcheck git
